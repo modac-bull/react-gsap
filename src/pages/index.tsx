c6c227f1c2f7cbcc909a6300ae7bddd01a84a6c5
leejihoon <ljhljh211@naver.com>
2023-10-31 09:38:25 +0900

    feat: GSAP 기본 애니메이션 코드 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import styled from '@emotion/styled/macro';
 import { motion } from 'framer-motion';
+import { gsap } from 'gsap';
+import { MutableRefObject, useLayoutEffect, useRef } from 'react';
 
 import HeadPageMeta from '@/components/layouts/HeadPageMeta';
 import LayoutDefault from '@/components/layouts/LayoutDefault';
 
 export default function HomePage() {
+  /* 
+    GSAP 애니메이션 사전 작업
+    - ref 설정
+    - useLayoutEffect 에서 호출
+    - (!! 단점 ) 요소가 많아지면 반복 작업해야 하는 이슈 있음
+  */
+  const boxRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!boxRef) return;
+    gsap.to(boxRef.current, { x: 300, rotation: '180deg' });
+  }, []);
+
   return (
     <>
       <HeadPageMeta title="React 애니메이션 라이브러리 리서치" />
@@ -19,6 +34,9 @@ export default function HomePage() {
           >
             <Box />
           </motion.div>
+
+          {/* GSAP */}
+          <Box ref={boxRef} />
         </Section>
 
         {/* Framer Motion - Scroll-trigger 애니메이션 */}
